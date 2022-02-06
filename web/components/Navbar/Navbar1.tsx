5244cddeaca1f3feb8c94de2031f02c821a06405
Andre Jarboe <andrejarboe@gmail.com>
2022-02-05 21:10:41 -0500

    add logo

--- web/components/Navbar/Navbar1.tsx
+++ web/components/Navbar/Navbar1.tsx
@@ -4,20 +4,23 @@ import Link from 'next/link'
 export default function Navbar1() {
   return (
     <nav>
-      <h1>header</h1>
       <div className="border-brand-black w-full border-b bg-white">
         <div className="mx-auto flex flex-col px-4 md:px-10 lg:flex-row lg:items-center lg:justify-between lg:px-20 2xl:px-40">
           <div className="">
-            <Link href="">
-              <a>
-                <Image
-                  src="https://placeholder.com/wp-content/uploads/2018/10/placeholder-1.png"
-                  alt="Picture of the author"
-                  layout="fill" // required
-                  objectFit="cover" // change to suit your needs
-                  className="object-top" // just an example
-                />
-              </a>
+            <Link href="/">
+              <div className="relative h-11 w-44">
+                <a href="">
+                  {/* DO NOT GORGE TO MAKE A PARENT DIV RELATIVE  */}
+                  <Image
+                    src="/lighthouse-logo.png"
+                    alt="Picture of the author"
+                    layout="fill" // required
+                    objectFit="contain" // change to suit your needs
+                    className="object-contain object-top" // just an example
+                  />
+                  {/* ^^^DO NOT GORGE TO MAKE A PARENT DIV RELATIVE  */}
+                </a>
+              </div>
             </Link>
           </div>
         </div>
