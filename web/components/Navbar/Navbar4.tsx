78c93aeb37c7f5e3396a66bba734394cfc20ff87
Andre Jarboe <andrejarboe@gmail.com>
2022-02-06 16:34:50 -0500

    nav4 logo done

--- web/components/Navbar/Navbar4.tsx
+++ web/components/Navbar/Navbar4.tsx
@@ -11,7 +11,7 @@ const data = {
     { name: 'Food Trucks', href: '/food-trucks' },
     { name: 'About', href: '/about' },
     { name: 'Blog', href: '/blog' },
-    { name: 'Blog', href: '/blog' },
+    { name: 'Contact', href: '/contact' },
     { name: 'Vendor sign up', href: '/vendor-sign-up' },
   ],
 }
@@ -21,16 +21,16 @@ export default function Navbar4() {
   const { sitemap } = data
 
   return (
-    <header>
+    <header className="border-brand-black md:border-b">
       {/* padding px-4 xl:px-0 */}
-      <div className="mx-auto max-w-7xl justify-between px-4 md:flex md:items-center md:justify-between xl:px-0">
-        <div className="mx-auto flex h-24 w-full items-center justify-between px-8">
+      <div className="mx-auto max-w-7xl justify-between md:flex md:items-center md:justify-between md:px-4 xl:px-0">
+        <div className="mx-auto flex h-24 w-full items-center justify-between px-4">
           {/* Logo  */}
           <Link href="/">
             <a
               href=""
               // Logo scaling md:h-full md:w-full
-              className=" relative h-20 w-44 md:h-full md:w-full"
+              className="relative flex h-20 w-44"
               //   className="relative h-20 w-44 drop-shadow-2xl md:flex md:justify-start"
               //   style={{ width: '100%', height: '100%', position: 'relative' }}
             >
@@ -40,7 +40,7 @@ export default function Navbar4() {
                 alt="logo"
                 layout="fill" // required
                 objectFit="contain" // change to suit your needs
-                className="object-contain object-center" // just an example
+                className="shrink justify-start object-contain object-center" // just an example
               />
               {/* ^^^DO NOT FORGET TO MAKE A PARENT DIV RELATIVE  */}
             </a>
@@ -49,7 +49,7 @@ export default function Navbar4() {
           {/* Mobile Button  */}
           <button
             onClick={() => setShow(!show)}
-            className="flex h-full items-center justify-center md:hidden"
+            className="flex h-full w-full items-center justify-end  md:hidden"
           >
             <svg
               className="stroke-primary hover:stroke-primary-300"
@@ -84,21 +84,51 @@ export default function Navbar4() {
           </button>
           {/* END Mobile Button  */}
         </div>
+        <div className="mx-auto h-[1px] w-[100vw] bg-brand-black md:hidden"></div>
         {/* Navbar */}
         <nav
           className={
             // ' flex-col md:block md:flex-row md:space-x-4 w-full md:flex-nowrap ' +
-            ' flex flex-col items-center justify-center text-center md:block md:flex-row md:space-x-6' +
+            // ' flex flex-col items-center justify-center text-center md:block md:flex-row md:space-x-4 ' +
+            'space-y flex flex-col items-center justify-end text-center capitalize  md:flex md:flex-row md:space-x-4 md:space-y-0' +
+            ' ' +
+            // 'flex h-full w-full items-center justify-end' +
+            // 'flex h-full w-full items-center justify-end  md:block' +
+            'mb:border-b-0 w-full pb-4 md:pb-0' +
             ' ' +
             (show ? 'block' : 'hidden')
           }
         >
           {sitemap.map((link, index, sitemap) => {
-            return (
-              <Link href={link.href} key={index}>
-                <a className="whitespace-nowrap">{link.name}</a>
-              </Link>
-            )
+            if (index === sitemap.length - 1) {
+              return (
+                //   <div className="div w-full">
+
+                // <Link href={link.href} key={index}>
+                //   <a className="hover:bg mt-4 whitespace-nowrap rounded-full border-primary bg-primary px-4 py-4 text-white hover:bg-white hover:text-primary md:mt-0  ">
+                //     {link.name}
+                //   </a>
+                // </Link>
+                //   </div>
+
+                <div className="w-full  border-b py-6 pb-6 md:border-b-0 md:p-0">
+                  <Link key={index} href={link.href}>
+                    <a className="whitespace-nowrap md:text-base rounded-full border-2 border-primary bg-primary p-3 text-lg font-medium text-white shadow transition duration-200 hover:bg-white hover:text-primary hover:shadow-xl ">
+                      {link.name}
+                    </a>
+                  </Link>
+                  {/* <div className="mt-8 -mx-[100%] h-[1px] overflow-visible bg-brand-black md:hidden"></div> */}
+                </div>
+              )
+            } else {
+              return (
+                <Link href={link.href} key={index}>
+                  <a className="w-full whitespace-nowrap border-b-2 px-4 py-4 text-lg font-medium text-brand-black hover:border-primary hover:text-primary hover:shadow md:border-white md:py-0 md:px-0 md:text-base md:hover:border-white md:hover:shadow-none ">
+                    {link.name}
+                  </a>
+                </Link>
+              )
+            }
           })}
         </nav>
         {/* END navbar  */}
