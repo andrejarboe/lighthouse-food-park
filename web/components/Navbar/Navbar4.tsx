2e223fca4b2631431dcd039bd9853a7e1d693179
Andre Jarboe <andrejarboe@gmail.com>
2022-02-06 13:33:58 -0500

    nav 4 functions mvp

--- web/components/Navbar/Navbar4.tsx
+++ web/components/Navbar/Navbar4.tsx
@@ -17,65 +17,85 @@ const data = {
 
 export default function Navbar4() {
   const [show, setShow] = useState(false)
-  const {sitemap} = data
+  const { sitemap } = data
 
   return (
     <header>
-      <div className="mx-auto">
-        {/* mobile  */}
-        <div className="">
-          {/* logo and button */}
-          <div className="flex justify-between px-4">
-            <Link href="/">
-              <a href="#_" className="relative h-20 w-44">
-                {/* DO NOT FORGET TO MAKE A PARENT DIV RELATIVE  */}
-                <Image
-                  src="/lighthouse-logo.png"
-                  alt="Picture of the author"
-                  layout="fill" // required
-                  objectFit="contain" // change to suit your needs
-                  className="object-contain object-center" // just an example
-                />
-                {/* ^^^DO NOT FORGET TO MAKE A PARENT DIV RELATIVE  */}
-              </a>
-            </Link>
-            {/* Mobile Button */}
-            <button onClick={() => setShow(!show)} className="">
-              <svg
-                className="stroke-primary hover:stroke-primary-300"
-                width="25"
-                height="18"
-                viewBox="0 0 25 18"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M1 1H19"
-                  stroke=""
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M6 9H24"
-                  stroke=""
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M1 17H19"
-                  stroke=""
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </button>
-          </div>
-          {/* end logo and button */}
+      {/* padding px-4 xl:px-0 */}
+      <div className="mx-auto max-w-7xl justify-between px-4 md:flex md:items-center md:justify-between xl:px-0">
+        <div className="mx-auto flex h-24 w-full items-center justify-between px-8">
+          {/* Logo  */}
+          <Link href="/">
+            <a
+              href=""
+              className="relative h-20 w-44 drop-shadow-2xl md:flex md:justify-start"
+            >
+              {/* DO NOT FORGET TO MAKE A PARENT DIV RELATIVE  */}
+              <Image
+                src="/lighthouse-logo.png"
+                alt="Picture of the author"
+                layout="fill" // required
+                objectFit="contain" // change to suit your needs
+                className="object-contain object-center " // just an example
+              />
+              {/* ^^^DO NOT FORGET TO MAKE A PARENT DIV RELATIVE  */}
+            </a>
+          </Link>
+          {/* END Logo  */}
+          {/* Mobile Button  */}
+          <button
+            onClick={() => setShow(!show)}
+            className="flex h-full items-center justify-center md:hidden"
+          >
+            <svg
+              className="stroke-primary hover:stroke-primary-300"
+              width="25"
+              height="18"
+              viewBox="0 0 25 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1 1H19"
+                stroke=""
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M6 9H24"
+                stroke=""
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M1 17H19"
+                stroke=""
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </button>
+          {/* END Mobile Button  */}
         </div>
-        {/* end mobile  */}
+        {/* Navbar */}
+        <nav
+          className={
+            'flex flex-col md:block md:flex-row md:space-x-4' +
+            ' ' +
+            (show ? 'block' : 'hidden')
+          }
+        >
+          <a href="" className="">
+            Home
+          </a>
+          <a href="" className="">
+            Schedule
+          </a>
+        </nav>
+        {/* END navbar  */}
       </div>
     </header>
   )
