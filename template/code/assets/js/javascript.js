46bae754526977964700e0ccdd08705e50d95ac7
Andre Jarboe <andrejarboe@gmail.com>
2022-02-05 19:01:12 -0500

    add files

---
+++ template/code/assets/js/javascript.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+            // 'primary':'#003C62',
+            // 'primary-50':'#1BA6FF',
+            // 'primary-100':'#069FFF',
+            // 'primary-200':'#0087DC',
+            // 'primary-300':'#006EB4',
+            // 'primary-400':'#00558B',
+            // 'primary-500':'#003C62',
+            // 'primary-600':'#00304E',
+            // 'primary-700':'#002339',
+            // 'primary-800':'#001725',
+            // 'primary-900':'#000A10',
+            'brand-lightblue':'rgba(53, 184, 190, 0.12)',
+            'brand-blue':'#35B8BE',
+            'brand-black':'#6A6A6A',
+            'brand-gray':'#616161',
+            'brand-gray-100':'#C4C4C4'
+        }
+      }
+    }
+}
