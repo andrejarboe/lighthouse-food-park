df27a74ef5eb9cb37850fae83c2e6349632505b7
Andre Jarboe <andrejarboe@gmail.com>
2022-02-05 21:15:49 -0500

    add menu button

--- web/components/Navbar/Navbar1.tsx
+++ web/components/Navbar/Navbar1.tsx
@@ -6,9 +6,9 @@ export default function Navbar1() {
     <nav>
       <div className="border-brand-black w-full border-b bg-white">
         <div className="mx-auto flex flex-col px-4 md:px-10 lg:flex-row lg:items-center lg:justify-between lg:px-20 2xl:px-40">
-          <div className="">
+          <div className="flex flex-row items-center justify-between py-4">
             <Link href="/">
-              <div className="relative h-11 w-44">
+              <div className="relative h-12 w-44">
                 <a href="">
                   {/* DO NOT GORGE TO MAKE A PARENT DIV RELATIVE  */}
                   <Image
@@ -22,6 +22,37 @@ export default function Navbar1() {
                 </a>
               </div>
             </Link>
+            <button className="focus:shadow-outline rounded-lg focus:outline-none lg:hidden">
+              <svg
+                width={25}
+                height={18}
+                viewBox="0 0 25 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1 1H19"
+                  stroke="#000"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M6 9H24"
+                  stroke="#000"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M1 17H19"
+                  stroke="#000"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
